4eab3b96b63d02d3c7440d685b5b74575fef90d8
Your Name <you@example.com>
2023-01-05 14:49:32 -0300

    adicionando aula de comandos de maior, menor e diferente

---
+++ repositoriodebanco/filtrandomaiormenorediferente.sql
@@ -0,0 +1,9 @@
+use sucos;
+select * from tabela_de_vendedores where nome = 'Claudia Morais';
+
+select * from tbcliente where idade >= 22;
+
+select * from tbcliente;
+select * from tbcliente where nome >= 'Gabriel Araujo';
+
+select * from tabela_de_vendedores where percentual_comissao > 0.10;
