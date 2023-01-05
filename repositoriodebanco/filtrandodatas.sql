585f777e7c22d3fa815565a65102e4a64fe93766
Your Name <you@example.com>
2023-01-05 15:41:05 -0300

    adicionando aula de filtro de datas

---
+++ repositoriodebanco/filtrandodatas.sql
@@ -0,0 +1,9 @@
+use sucos;
+select * from tbcliente;
+select * from tbcliente where data_nascimento >= '1995-01-13';
+
+select * from tbcliente where data_nascimento <= '1995-01-13';
+select * from tbcliente where year(data_nascimento) = '1995';
+select * from tbcliente where month(data_nascimento) = '9';
+select * from tbcliente where day(data_nascimento) = '1';
+select * from tbcliente where day(data_nascimento) = '20' and month(data_nascimento) = '12';
--- repositoriodebanco/filtrandomaiormenorediferente.sql
+++ repositoriodebanco/filtrandomaiormenorediferente.sql
@@ -7,3 +7,8 @@ select * from tbcliente;
 select * from tbcliente where nome >= 'Gabriel Araujo';
 
 select * from tabela_de_vendedores where percentual_comissao > 0.10;
+
+select * from tbproduto;
+select * from tbproduto where preco_lista between 6.30 and 6.32;
+
+
