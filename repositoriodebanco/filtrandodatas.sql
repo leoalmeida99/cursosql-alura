11d26894d8499554636b989e7b6b40466456c062
Your Name <you@example.com>
2023-01-05 15:51:28 -0300

    Adicionando a aula filtroscompostos.sql

--- repositoriodebanco/filtrandodatas.sql
+++ repositoriodebanco/filtrandodatas.sql
@@ -7,3 +7,4 @@ select * from tbcliente where year(data_nascimento) = '1995';
 select * from tbcliente where month(data_nascimento) = '9';
 select * from tbcliente where day(data_nascimento) = '1';
 select * from tbcliente where day(data_nascimento) = '20' and month(data_nascimento) = '12';
+
